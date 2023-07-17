a271a941f96dd40fed2d79995fcbe4fe5f040e9d
wilson12r <133931027+wilson12r@users.noreply.github.com>
2023-07-16 20:32:48 -0400

    Add files via upload

---
+++ Calculadora 2020-9878/Calculadora-main/Calculadora/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        double Numero1 = 0, Numero2 = 0;
+        char Operador;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void agregarNumero(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+
+            if (txtResultado.Text == "0")
+                txtResultado.Text = "";
+
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            Numero2 = Convert.ToDouble(txtResultado.Text);
+
+            if (Operador == '+')
+            {
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+                txtResultado.Text = (Numero1 + Numero2).ToString();
+                
+            }
+            else if (Operador == '−')
+            {
+
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+                txtResultado.Text = (Numero1 + Numero2).ToString();
+            }
+            else if (Operador == 'X')
+            {
+                
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+                txtResultado.Text = (Numero1 * Numero2).ToString();
+            }
+            else if (Operador == '∕')
+            {
+           
+                if (txtResultado.Text != "0")
+                {
+                
+                    Numero1 = Convert.ToDouble(txtResultado.Text);
+                    txtResultado.Text = (Numero1 / Numero2).ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No se puede dividir por cero!!");
+                }
+            }
+        }
+
+        private void btnQuitar_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text.Length > 1)
+            {
+                txtResultado.Text = txtResultado.Text.Substring(0, txtResultado.Text.Length - 1);
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
+        }
+
+        private void btnBorrarTodo_Click(object sender, EventArgs e)
+        {
+            Numero1 = 0;
+            Numero2 = 0;
+            Operador = '\0';
+            txtResultado.Text = "0";
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "0";
+        }
+
+        private void btnPunto_Click(object sender, EventArgs e)
+        {
+            if (!txtResultado.Text.Contains("."))
+            {
+                txtResultado.Text += ".";
+            }
+        }
+
+        private void btnSigno_Click(object sender, EventArgs e)
+        {
+         
+            Numero1 = Convert.ToDouble(txtResultado.Text);
+            Numero1 *= -1;
+            txtResultado.Text = Numero1.ToString();
+         
+
+         
+        }
+
+        private string Numero2Negativo()
+        {
+            Numero2 = Convert.ToDouble(txtResultado.Text);
+            Numero2 *= -1;
+            return txtResultado.Text = Numero2.ToString();
+        }
+
+        private void clickOperador(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            Numero1 = Convert.ToDouble(txtResultado.Text);
+            Operador = Convert.ToChar(boton.Tag);
+
+            if (Operador == '²')
+            {
+                Numero1 = Math.Pow(Numero1, 2);
+                txtResultado.Text = Numero1.ToString();
+            }
+            else if (Operador == '√')
+            {
+                Numero1 = Math.Sqrt(Numero1);
+                txtResultado.Text = Numero1.ToString();
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
+        }
+    }
+}
